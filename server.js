329d6b1e188253e98588aad858f50b5d19218a74
Samuel HOARAU <s.hoarau@student.isartdigital.com>
2024-05-30 12:24:36 +0200

    Add verification

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var urlencodedMiddlware = express.urlencoded();
 server.use(urlencodedMiddlware);
 server.set("port", portServer);
 
+// Charger les variables d'environnement
+require('dotenv').config();
+
 //Initialisation de body-parser
 const bodyParser = require("body-parser").json();
 
@@ -28,14 +31,12 @@ var pool = mysql.createPool({
 });
 
 
-server.get("/checkLicense", bodyParser, function (req, res) {
+server.post("/checkLicense", bodyParser, function (req, res) {
     var licenseCode = req.body.licenseCode;
 
     var request = "SELECT validationTime\
                    FROM code\
                    WHERE verificationCode = ?";
-
-    res.sendStatus(200);
 
     pool.query(request, [licenseCode], function (err, results, fields) {
         if (err != null) {
@@ -44,7 +45,22 @@ server.get("/checkLicense", bodyParser, function (req, res) {
             return;
         }
 
-        res.sendStatus(200);
+        if (results.length === 0) {
+            res.sendStatus(404); // Aucun résultat trouvé pour ce code de licence
+            return;
+        }
+
+        var validationTime = new Date(results[0].validationTime);
+        var currentTime = new Date();
+
+        console.log("validationTime" + validationTime);
+        console.log("currentTime" + currentTime);
+
+        if (validationTime > currentTime) {
+            res.sendStatus(200); // Le temps de validation est antérieur à la date actuelle
+        } else {
+            res.sendStatus(400); // Le temps de validation est postérieur à la date actuelle
+        }
     });
 });
 
